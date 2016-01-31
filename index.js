924a82263472b404a0d1c11ae9a4cd434a3df6de
Yury Liavitski <yury.liavitski@il111.ice.local>
2016-01-31 22:19:34 +0100

    reduce in terms of flatmap

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ function Signal(expr) {
   }
 
   self.reduce = function(func, start) {
-    return new Signal((x) => {
-      start = func(self.apply(x), start);
+    return self.map((x) => {
+      start = func(x, start);
       return start;
-    });
+    })
   }
 
   self.send(expr);
@@ -61,7 +61,7 @@ var a = new Signal(1);
 var b = new Signal(2);
 
 var twice = a.zip(b, (x, y) => x + y);
-var reduced = b.reduce((x,y) => x+y, 0);
+var reduced = b.red((x,y) => x+y, 0);
 console.log(reduced.myValue);
 b.send(3);
 console.log(reduced.myValue);
